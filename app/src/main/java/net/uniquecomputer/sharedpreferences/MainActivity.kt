6b1431f15586e75c099923b64869b616c5cee4d5
Sagar Dhadke <peaceunique2003@gmail.com>
2023-06-22 16:13:42 +0530

    Added Binding Code

--- app/src/main/java/net/uniquecomputer/sharedpreferences/MainActivity.kt
+++ app/src/main/java/net/uniquecomputer/sharedpreferences/MainActivity.kt
@@ -2,13 +2,15 @@ package net.uniquecomputer.sharedpreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.uniquecomputer.sharedpreferences.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+   private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
-        //test Commit
     }
 }
